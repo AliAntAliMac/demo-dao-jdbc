b49d9fcd891968a6b96d21e0b6d0dc19cf2a70c6
AAAM <aaam@gmail.com>
2026-03-29 21:40:31 +0200

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.entinties.Department;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "books");
+
+        System.out.println(obj);
+    }
+}
